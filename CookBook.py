bc5340831dbb7f2add903caea0d4fc71e2b32226
Benjy <108727379+Benjy0812@users.noreply.github.com>
2025-03-25 16:33:34 +0100

    Add console clearing functionality and improve user experience in menu navigation

--- CookBook.py
+++ CookBook.py
@@ -2,6 +2,12 @@ import json
 import time
 import os
 
+def clear_console():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
 recipes = []
 
 
@@ -27,9 +33,10 @@ def add_recipe():
             print("\nOne or more fields are empty. Recipe was not added, please try again.\n")
             continue
         else:
+            clear_console()
             recipe = (name, ingredients, instructions, servings)
             recipes.append(recipe)
-            print("Recipe added successfully!\n")
+            print("\nRecipe added successfully!\n")
             return recipe
 
 
@@ -42,24 +49,30 @@ def view_recipe(recipe):
     print(f"Ingredients: {ingredients}")
     print(f"Instructions: {instructions}")
     print(f"Servings: {servings}")
-    print("----------------------\n")
+    print("----------------------")
 
 
 def view_all_recipes():
     if not recipes:
-        print("No recipes available.")
+        clear_console()
+        print("\nNo recipes available.\n")
         return
 
     for i, recipe in enumerate(recipes, start=1):
         print(f"\n----- Recipe {i} -----")
         view_recipe(recipe)
+    
+    input("Press enter to continue.")
+    clear_console()
 
 
 def search_recipes():
     print("Search Recipes:")
+    print("Not added yet wont work.")
 
 
 def save_and_exit():
+    clear_console()
     print("saving...")
     with open("recipes.json", "w") as f:
         json.dump(recipes, f)
@@ -72,21 +85,24 @@ def save_and_exit():
 def menu():
     while True:
         print("CookBook Menu:")
-        print("1. Add Recipe")
-        print("2. View All Recipes")
-        print("3. Search Recipes not implemented")
-        print("4. Save and Exit")
+        print("1. Add Recipe.")
+        print("2. View All Recipes.")
+        print("3. Search Recipes not implemented yet.")
+        print("4. Save and Exit.\n")
 
         choice = input("Enter your choice: ")
 
         try:
             if choice == "1":
+                clear_console()
                 add_recipe()
             elif choice == "2":
+                clear_console()
                 view_all_recipes()
             elif choice == "3":
                 search_recipes()
             elif choice == "4":
+                clear_console()
                 save_and_exit()
         except ValueError as e:
             print(e)
